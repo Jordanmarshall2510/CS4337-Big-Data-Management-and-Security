d5206b4742c4789db8068ed661a0fb21fc98b071
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2021-11-24 01:34:01 +0000

    push

--- app.py
+++ app.py
@@ -1,112 +1,121 @@
-## The dash code will be here
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 from dash import dcc
 from dash import html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
-import plotly.tools as tls
 import datetime
 
 # Disable future warnings
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-date = datetime.date.today()-datetime.timedelta(days=365)
-date = date.strftime("%m-%d-%Y")
-path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(date)
-df = pd.read_csv(path)
+def get_graphs(date):
+    path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(date)
+    df = pd.read_csv(path)
 
-df.drop(['FIPS', 'Admin2','Last_Update','Province_State', 'Combined_Key'], axis=1, inplace=True)
-df.rename(columns={'Country_Region': "Country"}, inplace=True)
-world = df.groupby("Country")['Confirmed','Active','Recovered','Deaths'].sum().reset_index()
-world.head(10)
+    df.drop(['FIPS', 'Admin2','Last_Update','Province_State', 'Combined_Key'], axis=1, inplace=True)
+    df.rename(columns={'Country_Region': "Country"}, inplace=True)
+    world = df.groupby("Country")['Confirmed','Active','Recovered','Deaths'].sum().reset_index()
+    world.head(10)
 
-data = world.drop(["Active", "Recovered"], axis=1)
-figScatter = px.scatter(data, x="Confirmed", y="Deaths", hover_data=["Country"], color="Confirmed", title="Interactive Scatter Map w/ Confirmed and Death Cases", template="plotly_dark")
+    data = world.drop(["Active", "Recovered"], axis=1)
+    figScatter = px.scatter(data, x="Confirmed", y="Deaths", hover_data=["Country"], color="Confirmed", title="Interactive Scatter Map w/ Confirmed and Death Cases", template="plotly_dark")
 
-figMap = px.choropleth(world,locations="Country", locationmode="country names", color="Confirmed", hover_name="Country",title="Interactive Choropleth Map w/ Confirmed Cases", template='plotly_dark', )
+    figMap = px.choropleth(world,locations="Country", locationmode="country names", color="Confirmed", hover_name="Country",title="Interactive Choropleth Map w/ Confirmed Cases", template='plotly_dark', )
 
-### Find top 20 countries with maximum number of confirmed cases
-top_20 = world.sort_values(by=['Confirmed'], ascending=False).head(20)
+    ### Find top 20 countries with maximum number of confirmed cases
+    top_20 = world.sort_values(by=['Confirmed'], ascending=False).head(20)
 
-### Generate a Barplot
-fig = plt.figure()
-plot = sns.barplot(top_20['Confirmed'], top_20['Country'])
-for i,(value,name) in enumerate(zip(top_20['Confirmed'],top_20['Country'])):
-    plot.text(value,i-0.05,name + ":\t" + f'{value:,.0f}')
-barplotConfirmed = tls.mpl_to_plotly(fig)
+    ### Generate a Barplot
+    barplotConfirmed = px.bar(top_20, x='Confirmed', y='Country', template="plotly_dark")
 
-### Find top 20 countries with maximum number of confirmed cases
-top_20 = world.sort_values(by=['Deaths'], ascending=False).head(20)
+    ### Find top 20 countries with maximum number of confirmed cases
+    top_20 = world.sort_values(by=['Deaths'], ascending=False).head(20)
 
-### Generate a Barplot
-fig = plt.figure()
-plot = sns.barplot(top_20['Deaths'], top_20['Country'])
-for i,(value,name) in enumerate(zip(top_20['Deaths'],top_20['Country'])):
-    plot.text(value,i-0.05,name + ":\t" + f'{value:,.0f}')
-barplotDeaths = tls.mpl_to_plotly(fig)
+    ### Generate a Barplot
+    barplotDeaths = px.bar(top_20, x='Deaths', y='Country', template="plotly_dark")
+    
+    return figScatter, figMap, barplotConfirmed, barplotDeaths
 
-external_stylesheets =  ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets =  ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.DARKLY]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title='COVID-19 Visualisation')
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
 app.layout = html.Div(children=[
-                html.H1(children='COVID-19 Visualisation w/ Plotly Dash'),
-
+                html.Div([
+                    html.H1(children='COVID-19 Visualisation using Plotly Dash'),
+                ], className='row', style={'text-align':'center', 'padding':'10px'}) ,
+                html.Div([
+                    html.H3('Pick date to view:'),
+                    dcc.DatePickerSingle(
+                        id='date-picker',
+                        display_format = 'DD/MM/YYYY',
+                        min_date_allowed=datetime.date(2020, 3, 22),
+                        max_date_allowed= datetime.date.today() - datetime.timedelta(days=2),
+                        date= datetime.date.today() - datetime.timedelta(days=2),
+                        style={'font-size':'15px'},
+                    ),
+                ], className='row', style={'text-align':'center'}) ,
+                html.Div([
+                    html.H3(id='output-data'),
+                ], className='row', style={'text-align':'center', 'padding-top':'15px'}) ,
                 html.Div([
                     html.Div([
-
-                        html.H3(children='Interactive Scatter Map w/ Confirmed and Death Cases'),
+                        html.H2(children='Interactive Scatter Map w/ Confirmed and Death Cases'),
                         dcc.Graph(
                             id='scatterPlot',
-                            figure=figScatter
                         ), 
-
-                    ], className='six columns userInput'),
+                    ], className='six columns userInput', style={'text-align':'center'}),
                     html.Div([
-
-                        html.H3(children='Interactive Choropleth Map w/ Confirmed Cases'),
+                        html.H2(children='Interactive Choropleth Map w/ Confirmed Cases'),
                         dcc.Graph(
                             id='mapPlot',
-                            figure=figMap
                         ),  
-
-                    ], className='six columns'),
-                ], className='row'),
+                    ], className='six columns', style={'text-align':'center'}),
+                ], className='row', style={'padding':'15px'}),
 
                 html.Div([
                     html.Div([
-
-                        html.H3(children='Top 20 Countries (Confirmed Cases)'),
+                        html.H2(children='Top 20 Countries (Confirmed Cases)'),
                         dcc.Graph(
-                            id='plot1',
-                            figure=barplotConfirmed
+                            id='barConfirmed',
                         ), 
-
-                    ], className='six columns userInput'),
+                    ], className='six columns', style={'text-align':'center'}),
                     html.Div([
-
-                        html.H3(children='Top 20 Countries (Death Cases)'),
+                        html.H2(children='Top 20 Countries (Death Cases)'),
                         dcc.Graph(
-                            id='plot2',
-                            figure=barplotDeaths
+                            id='barDeaths',
                         ),  
-
-                    ], className='six columns'),
-                ], className='row'),
+                    ], className='six columns', style={'text-align':'center'}),
+                ], className='row', style={'padding':'15px'}),
             ])
+
+@app.callback(
+    Output('output-data', 'children'),
+    Output('scatterPlot', 'figure'),
+    Output('mapPlot', 'figure'),
+    Output('barConfirmed', 'figure'),
+    Output('barDeaths', 'figure'),
+    Input('date-picker', 'date'))
+def update_output(date_value):
+    selection = 'Date Selected: '
+    if date_value is not None:
+        date_object = datetime.date.fromisoformat(date_value)
+        date_string = date_object.strftime('%m-%d-%Y')
+        date_text_string = date_object.strftime('%B %d, %Y')
+
+        try:
+            path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(date_string)
+            pd.read_csv(path)
+        except:
+              return selection + 'Data Not Available', px.bar(), px.bar(), px.bar(), px.bar(), 
+
+        scatterPlot, mapPlot, barConfirmed, barDeaths = get_graphs(date_string)
+        
+        return selection + date_text_string, scatterPlot, mapPlot, barConfirmed, barDeaths
 
 if __name__ == '__main__':
     app.run_server(debug=True)
